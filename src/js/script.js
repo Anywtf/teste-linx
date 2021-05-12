9207a6968d95cf97655a7f92d6115a91174fd20e
Anywtf <apnmacedo@gmail.com>
2021-05-12 15:50:23 -0300

    add project

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+
+document.getElementById("showHover").addEventListener("mouseover", mouseOver);
+document.getElementById("showHover").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+  document.getElementById("show").style.display = "block";
+}
+
+function mouseOut() {
+  document.getElementById("show").style.display = "none";
+}
+
+
+
+
+
